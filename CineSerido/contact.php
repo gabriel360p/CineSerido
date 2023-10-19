d59bc8d51b0fc4720cc8f4d6bdcb75885f01c028
lorium17 <109006007+gabriel360p@users.noreply.github.com>
2023-10-19 09:52:19 -0300

    background

--- CineSerido/contact.php
+++ CineSerido/contact.php
@@ -50,7 +50,7 @@ https://templatemo.com/tm-584-pod-talk
 
                     <div class="col-lg-12 col-12 text-center">
 
-                        <h2 class="mb-0">Contact Page</h2>
+                        <h2 class="mb-0">Contato</h2>
                     </div>
 
                 </div>
@@ -64,10 +64,10 @@ https://templatemo.com/tm-584-pod-talk
 
                     <div class="col-lg-5 col-12 pe-lg-5">
                         <div class="contact-info">
-                            <h3 class="mb-4">We love to help you. Get in touch</h3>
+                            <h3 class="mb-4">Adoramos ajudar você. Entrer em contato:</h3>
 
                             <p class="d-flex border-bottom pb-3 mb-4">
-                                <strong class="d-inline me-4">Phone:</strong>
+                                <strong class="d-inline me-4">Telefone:</strong>
                                 <span>010-020-0340</span>
                             </p>
 
@@ -77,8 +77,9 @@ https://templatemo.com/tm-584-pod-talk
                             </p>
 
                             <p class="d-flex">
-                                <strong class="d-inline me-4">Location:</strong>
-                                <span>1355 Market Street, Downtown San Francisco, California</span>
+                                <strong class="d-inline me-4">Localização:</strong>
+                                <!-- <span>1355 Market Street, Downtown San Francisco, California</span> -->
+                                <span>Rua João Luizo, Rio Grande do Norte, Caicó</span>
                             </p>
                         </div>
                     </div>
@@ -100,7 +101,7 @@ https://templatemo.com/tm-584-pod-talk
 
                     <div class="col-lg-8 col-12 mx-auto">
                         <div class="section-title-wrap mb-5">
-                            <h4 class="section-title">You know, Contact Form</h4>
+                            <h4 class="section-title">Formulário de Contato:</h4>
                         </div>
 
                         <form action="#" method="post" class="custom-form contact-form" role="form">
@@ -110,7 +111,7 @@ https://templatemo.com/tm-584-pod-talk
                                         <input type="text" name="full-name" id="full-name" class="form-control"
                                             placeholder="Full Name" required="">
 
-                                        <label for="floatingInput">Full Name</label>
+                                        <label for="floatingInput">Nome Completo</label>
                                     </div>
                                 </div>
 
@@ -119,7 +120,7 @@ https://templatemo.com/tm-584-pod-talk
                                         <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*"
                                             class="form-control" placeholder="Email address" required="">
 
-                                        <label for="floatingInput">Email address</label>
+                                        <label for="floatingInput">Email</label>
                                     </div>
                                 </div>
 
@@ -128,19 +129,19 @@ https://templatemo.com/tm-584-pod-talk
                                         <input type="text" name="company" id="name" class="form-control"
                                             placeholder="Name" required="">
 
-                                        <label for="floatingInput">Company</label>
+                                        <label for="floatingInput">Empresa (opcional)</label>
                                     </div>
 
                                     <div class="form-floating">
                                         <textarea class="form-control" id="message" name="message"
                                             placeholder="Describe message here"></textarea>
 
-                                        <label for="floatingTextarea">Describe message here</label>
+                                        <label for="floatingTextarea">Mensagem</label>
                                     </div>
                                 </div>
 
                                 <div class="col-lg-4 col-12 ms-auto">
-                                    <button type="submit" class="form-control">Submit</button>
+                                    <button type="submit" class="form-control">Enviar</button>
                                 </div>
 
                             </div>
@@ -153,109 +154,8 @@ https://templatemo.com/tm-584-pod-talk
     </main>
 
 
-    <footer class="site-footer">
-        <div class="container">
-            <div class="row">
+    <?php require 'footer.html'; ?>
 
-                <div class="col-lg-6 col-12 mb-5 mb-lg-0">
-                    <div class="subscribe-form-wrap">
-                        <h6>Subscribe. Every weekly.</h6>
-
-                        <form class="custom-form subscribe-form" action="#" method="get" role="form">
-                            <input type="email" name="subscribe-email" id="subscribe-email" pattern="[^ @]*@[^ @]*"
-                                class="form-control" placeholder="Email Address" required="">
-
-                            <div class="col-lg-12 col-12">
-                                <button type="submit" class="form-control" id="submit">Subscribe</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-12 mb-4 mb-md-0 mb-lg-0">
-                    <h6 class="site-footer-title mb-3">Contact</h6>
-
-                    <p class="mb-2"><strong class="d-inline me-2">Phone:</strong> 010-020-0340</p>
-
-                    <p>
-                        <strong class="d-inline me-2">Email:</strong>
-                        <a href="#">inquiry@pod.co</a>
-                    </p>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-12">
-                    <h6 class="site-footer-title mb-3">Download Mobile</h6>
-
-                    <div class="site-footer-thumb mb-4 pb-2">
-                        <div class="d-flex flex-wrap">
-                            <a href="#">
-                                <img src="images/app-store.png" class="me-3 mb-2 mb-lg-0 img-fluid" alt="">
-                            </a>
-
-                            <a href="#">
-                                <img src="images/play-store.png" class="img-fluid" alt="">
-                            </a>
-                        </div>
-                    </div>
-
-                    <h6 class="site-footer-title mb-3">Social</h6>
-
-                    <ul class="social-icon">
-                        <li class="social-icon-item">
-                            <a href="#" class="social-icon-link bi-instagram"></a>
-                        </li>
-
-                        <li class="social-icon-item">
-                            <a href="#" class="social-icon-link bi-twitter"></a>
-                        </li>
-
-                        <li class="social-icon-item">
-                            <a href="#" class="social-icon-link bi-whatsapp"></a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-        </div>
-
-        <div class="container pt-5">
-            <div class="row align-items-center">
-
-                <div class="col-lg-2 col-md-3 col-12">
-                    <a class="navbar-brand" href="index.html">
-                        <img src="images/pod-talk-logo.png" class="logo-image img-fluid" alt="templatemo pod talk">
-                    </a>
-                </div>
-
-                <div class="col-lg-7 col-md-9 col-12">
-                    <ul class="site-footer-links">
-                        <li class="site-footer-link-item">
-                            <a href="#" class="site-footer-link">Homepage</a>
-                        </li>
-
-                        <li class="site-footer-link-item">
-                            <a href="#" class="site-footer-link">Browse episodes</a>
-                        </li>
-
-                        <li class="site-footer-link-item">
-                            <a href="#" class="site-footer-link">Help Center</a>
-                        </li>
-
-                        <li class="site-footer-link-item">
-                            <a href="#" class="site-footer-link">Contact Us</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-3 col-12">
-                    <p class="copyright-text mb-0">Copyright © 2036 Talk Pod Company
-                        <br><br>
-                        Design: <a rel="nofollow" href="https://templatemo.com/page/1" target="_parent">TemplateMo</a>
-                    </p> Distribution: <a rel="nofollow" href="https://themewagon.com" target="_blank">ThemeWagon</a>
-                </div>
-            </div>
-        </div>
-    </footer>
 
     <!-- JAVASCRIPT FILES -->
     <script src="js/jquery.min.js"></script>
